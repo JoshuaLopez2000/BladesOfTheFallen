6921efd5a94973b0b989e37449c3e8bfe1007ff7
JoshuaLopez2000 <54430949+JoshuaLopez2000@users.noreply.github.com>
2025-08-08 05:35:15 -0600

    Lighting Generated

--- Assets/Project/Scripts/ChangeMainScreen.cs
+++ Assets/Project/Scripts/ChangeMainScreen.cs
@@ -1,21 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class ChangeMainScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //if new input sistem touch screen is pressed change the main screen
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed)
         {
-            ChangeScreen();
+            if (Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+            {
+                ChangeScreen();
+            }
         }
     }
 
