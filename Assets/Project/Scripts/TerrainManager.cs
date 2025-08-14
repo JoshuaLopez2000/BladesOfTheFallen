ffbce860190d33de1f80c6dd2fe2202895a2a95a
JoshuaLopez2000 <54430949+JoshuaLopez2000@users.noreply.github.com>
2025-08-14 01:27:40 -0600

    Automatic terrain instantiation

--- Assets/Project/Scripts/TerrainManager.cs
+++ Assets/Project/Scripts/TerrainManager.cs
@@ -3,14 +3,36 @@ using UnityEngine;
 public class TerrainManager : MonoBehaviour
 {
     public GameObject leftTerrainPrefab, mainTerrainPrefab, rightTerrainPrefab;
+    public Transform player;
+    private float terrainWidth;
+    private float lastRightX;
+    private float lastLeftX;
 
     void Start()
     {
-        float terrainWidth = GetTotalWidth(mainTerrainPrefab);
+        terrainWidth = GetTotalWidth(mainTerrainPrefab);
 
         Instantiate(leftTerrainPrefab, Vector3.left * terrainWidth, Quaternion.identity);
         Instantiate(mainTerrainPrefab, Vector3.zero, Quaternion.identity);
         Instantiate(rightTerrainPrefab, Vector3.right * terrainWidth, Quaternion.identity);
+
+        lastRightX = terrainWidth;
+        lastLeftX = -terrainWidth;
+    }
+
+    void Update()
+    {
+        if (player.position.x > lastRightX - (terrainWidth / 2))
+        {
+            lastRightX += terrainWidth;
+            Instantiate(rightTerrainPrefab, new Vector3(lastRightX, 0, 0), Quaternion.identity);
+        }
+
+        if (player.position.x < lastLeftX + (terrainWidth / 2))
+        {
+            lastLeftX -= terrainWidth;
+            Instantiate(leftTerrainPrefab, new Vector3(lastLeftX, 0, 0), Quaternion.identity);
+        }
     }
 
     float GetTotalWidth(GameObject prefab)
@@ -22,13 +44,11 @@ public class TerrainManager : MonoBehaviour
             return 0f;
         }
 
-        // Combinar todos los bounds
         Bounds combinedBounds = renderers[0].bounds;
         for (int i = 1; i < renderers.Length; i++)
         {
             combinedBounds.Encapsulate(renderers[i].bounds);
         }
-
         return combinedBounds.size.x;
     }
 }
